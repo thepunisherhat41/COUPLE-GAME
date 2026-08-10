8b1a50c62b8bd74fa4bd833812019bcb6524d678
thepunisherhat41 <thepunisherhat@gmail.com>
2026-08-09 22:21:26 -0300

    load expanded hot couple deck

--- bootstrap.js
+++ bootstrap.js
@@ -1,9 +1,10 @@
-import { triviaQuestions } from './data.js';
+import { coupleQuestions, triviaQuestions } from './data.js';
 import { popularEasy } from './questions/popular-easy.js';
 import { popularMedium } from './questions/popular-medium.js';
 import { popularHardA } from './questions/popular-hard-a.js';
 import { popularHardB } from './questions/popular-hard-b.js';
 import { triviaCorrections } from './questions/corrections.js';
+import { extraHotCoupleQuestions } from './questions/couple-hot.js';
 
 const popularTriviaQuestions = [
   ...popularEasy,
@@ -17,7 +18,17 @@ for (const question of popularTriviaQuestions) {
   if (correction) Object.assign(question, correction);
 }
 
-// Mantém o motor do jogo original, mas substitui integralmente o baralho antigo.
+// Amplia o modo Quente sem alterar os modos Leve e Profundo.
+// O motor continua sorteando perguntas sem repetição enquanto houver perguntas no baralho.
+const existingCoupleIds = new Set(coupleQuestions.map((question) => question.id));
+for (const question of extraHotCoupleQuestions) {
+  if (!existingCoupleIds.has(question.id)) {
+    coupleQuestions.push(question);
+    existingCoupleIds.add(question.id);
+  }
+}
+
+// Mantém o motor do jogo original, mas substitui integralmente o baralho antigo de trivia.
 // Como arrays importados são objetos mutáveis, app.js recebe a mesma instância já atualizada.
 triviaQuestions.splice(0, triviaQuestions.length, ...popularTriviaQuestions);
 
